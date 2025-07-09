725af19a4264028ac45150fc896f20aafb120a28
Baque2005 <155196295+Baque2005@users.noreply.github.com>
2025-07-08 23:58:46 -0500

    Mensaje claro describiendo los cambios realizadoss

--- src/index.js
+++ src/index.js
@@ -23,7 +23,7 @@ app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')));
 app.use(express.static(path.join(__dirname, '..', 'build')));
 
 // Para cualquier otra ruta no definida en API, enviar el index.html de React
-app.get('*', (req, res) => {
+app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, '..', 'build', 'index.html'));
 });
 
