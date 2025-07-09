de174482dd40e5b0091a798a08bc1153f558f524
Baque2005 <155196295+Baque2005@users.noreply.github.com>
2025-07-08 22:25:59 -0500

    Mensaje claro describiendo los cambios realizados

--- src/routes.js
+++ src/routes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const path = require('path');
 const fs = require('fs');
-const pool = require('../db');
+const pool = require('./db');
 
 const {
   crearTarea,
